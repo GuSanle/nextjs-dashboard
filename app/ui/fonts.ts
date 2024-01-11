48ab4ec68162f6fbdffc976356d57693dbc79688
slgu <slgu@cybozu.cn>
2024-01-11 10:43:46 +0800

    add fonts folder

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,6 +2,6 @@ import localFont from 'next/font/local';
 
 // Font files can be colocated inside of `app`
 export const aliFont = localFont({
-  src: '../../public/AlibabaPuHuiTi-3-65-Medium.woff2',
+  src: '../../public/fonts/AlibabaPuHuiTi-3-65-Medium.woff2',
   display: 'swap',
 });
