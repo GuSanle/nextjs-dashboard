dbf079785adce78d28cef6efd91b78fd662fd89f
slgu <slgu@cybozu.cn>
2024-01-15 09:37:44 +0800

    session auth获取demo

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,17 @@
 import { Metadata } from 'next';
+import { auth } from '@/auth';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page() {
+  const session = await auth();
+
+  return (
+    <>
+      <div>name:{session?.user?.name}</div>
+      <div>email:{session?.user?.email}</div>
+    </>
+  );
 }
